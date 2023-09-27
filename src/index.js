5ed199ad38453507f956f533e08ba88e9f2f1eb4
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-09-27 15:52:38 +0300

    Updated views

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 
+const routes = require('./routes.js');
+
 const config = require('./config.js');
 const setupViewEngine = require('./config/viewEngine.js');
 
 const app = express();
 setupViewEngine(app);
 
-app.use(express.static('src/public'))
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use(express.static('src/public'));
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is listening on PORT: ${config.PORT}...`));
