b9af34769c0c7e1230190b272b68a579e940a4c3
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-09-28 19:14:18 +0300

    Added search functionality

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,7 +2,22 @@ const Cube = require('../models/Cube.js');
 const db = require('../db.json');
 
 const getFrontPage = (req, res) => {
-    res.render('index', {cubes: db.cubes});
+    const {search, from, to} = req.query;
+    let cubes = db.cubes;
+
+    if(search){
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if(from){
+        cubes = cubes.filter(cube => cube.difficultyLevel >= from)
+    }
+
+    if(to){
+        cubes = cubes.filter(cube => cube.difficultyLevel <= to);
+    }
+
+    res.render('index', {cubes, search, from, to});
 }
 
 const getCreateCube = (req, res) => {
