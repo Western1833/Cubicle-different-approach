84f0011e8dd7d33bb338707dbb2443d45c1ecf50
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-04 21:56:53 +0300

    Accessory form is saved on DB

--- src/controllers/accessoryControler.js
+++ src/controllers/accessoryControler.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
+const Accessory = require('../models/Accessory.js');
+
 router.get('/create', (req, res) => {
     res.render('createAccessory');
 });
+
+router.post('/create', async (req, res) => {
+    const {name, description, imageUrl} = req.body;
+
+    const accessory = new Accessory({name, description, imageUrl});
+    await accessory.save();
+    res.redirect('/');
+})
 
 module.exports = router;
