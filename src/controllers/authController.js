922021f3d53545fe9d03919578be5efc6e4a79fd
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-10 14:21:22 +0300

    Updates to user model, authController and new file authService

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const authService = require('../services/authService.js');
 
 router.get('/login', (req, res) => {
     res.render('loginPage');
@@ -8,12 +9,20 @@ router.get('/register', (req, res) => {
     res.render('registerPage');
 });
 
-router.post('register', (req, res) => {
+router.post('register', async (req, res) => {
     const {username, password, repeatPassword} = req.body;
 
     if(password !== repeatPassword){
         throw alert("Passwords don't match!");
     }
+
+    const existingUser = await authService.getUserByUsername(username);
+
+    if(existingUser){
+        throw alert('The username you chose already exists, please pick another.');
+    }
+
+    const user = await authService.register(username, password);
 });
 
 module.exports = router;
--- src/models/User.js
+++ src/models/User.js
@@ -8,7 +8,10 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        
+        required: true,        
     }
 });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
