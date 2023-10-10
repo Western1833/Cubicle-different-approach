582657b319a07c76299bf4a670f3f1646a1050ec
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-10 22:26:20 +0300

    Updated authMiddleware and added it to routes

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -8,11 +8,20 @@ exports.authentication = async(req, res, next) => {
         try{
             const decodedToken = await jwt.verify(token, config.SECRET);
             req.user = decodedToken;
+            req.isAuthenticated = true;
         }catch(err){
             console.log(err);
             res.clearCookie('auth');
             res.redirect('404');
         }
+    }
+
+    next();
+};
+
+exports.isAuthenticated = (req, res, next) => {
+    if(!req.isAuthenticated){
+        return res.redirect('/login');
     }
 
     next();
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,15 @@ const router = require('express').Router();
 const cubeController = require('./controllers/cubeController.js');
 const accessoryControler = require('./controllers/accessoryControler.js');
 const authController = require('./controllers/authController.js');
+const {isAuthenticated} = require('./middlewares/authMiddleware.js');
 
 router.get('/', cubeController.getFrontPage);
 
-router.get('/create', cubeController.getCreateCube);
+router.get('/create', isAuthenticated, cubeController.getCreateCube);
 
 router.get('/about', cubeController.getAboutPage);
 
-router.post('/create', cubeController.postCreateCube);
+router.post('/create', isAuthenticated, cubeController.postCreateCube);
 
 router.get('/details/:id', cubeController.getCubeDetails);
 
