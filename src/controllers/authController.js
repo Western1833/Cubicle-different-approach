84a938c709872d4c2907379a321465f1958d90c8
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-10-10 15:49:28 +0300

    Registration now works + bcrypt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,20 +9,26 @@ router.get('/register', (req, res) => {
     res.render('registerPage');
 });
 
-router.post('register', async (req, res) => {
+router.post('/register', async (req, res) => {
     const {username, password, repeatPassword} = req.body;
 
     if(password !== repeatPassword){
-        throw alert("Passwords don't match!");
+        const message = "Passwords don't match!";
+        return res.json({ message });
     }
 
     const existingUser = await authService.getUserByUsername(username);
 
     if(existingUser){
-        throw alert('The username you chose already exists, please pick another.');
+        const message = "The username you chose already exists, please pick another.";
+        return res.json({message});
     }
 
     const user = await authService.register(username, password);
+
+    console.log(user);
+
+    res.redirect('/login');
 });
 
 module.exports = router;
--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        minLength: 3
+        minLength: [3, 'Username is too short.']
     },
     password: {
         type: String,
-        required: true,        
+        required: true,
+        minLength: [4, 'Password is too short.']    
     }
+});
+
+userSchema.pre('save', function(next){
+    bcrypt.hash(this.password, 10)
+    .then(hash => {
+        this.password = hash;
+        next();
+    });
 });
 
 const User = mongoose.model('User', userSchema);
